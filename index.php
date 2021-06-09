5eaeac79731faabd183b5e35c51ac378f5881bd6
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 14:56:18 +0200

    evo

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang='fr'>
+	<head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>PHP ex 2.8 condition</title>
+    </head>
+    <body >
+        <p>
+            <?php
+                $idx=0;
+                for ($idx=0; $idx < 10; $idx++)
+                {
+                    echo $idx.' ';
+                }
+           ?>
+       </p>
+    </body>
+</html>
